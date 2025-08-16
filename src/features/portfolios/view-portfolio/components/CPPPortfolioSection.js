5f64ead6d3fd7daa84f05faacc46ba93a85ec52f
MuhamanButt <muhamanibutt@gmail.com>
2025-08-16 17:27:50 +0500

    fix : minor bug fix

--- src/features/portfolios/view-portfolio/components/CPPPortfolioSection.js
+++ src/features/portfolios/view-portfolio/components/CPPPortfolioSection.js
@@ -12,26 +12,31 @@ const CPPPPortfolioSection = ({ CompanyData }) => {
 
   return (
     <section className="portfolio-section" id="services">
-      <div className="portfolio-container">
-        <h2 className="portfolio-heading">More Services</h2>
+      {services?.length > 0 && (
+        <div className="portfolio-container">
+          <h2 className="portfolio-heading">More Services</h2>
 
-        <Row gutter={[24, 24]}>
-          {services?.slice(0, visibleCount).map((service, index) => (
+          <Row gutter={[24, 24]}>
+            {services?.slice(0, visibleCount).map((service, index) => (
               <Col xs={24} lg={12} key={index}>
                 <ServiceCard service={service} key={service?.id} />
               </Col>
-          ))}
-        </Row>
+            ))}
+          </Row>
 
-        {/* Show More button */}
-        {visibleCount < services.length && (
-          <div className="portfolio-showmore-container">
-            <button className="portfolio-showmore-btn" onClick={handleShowMore}>
-              Show More
-            </button>
-          </div>
-        )}
-      </div>
+          {/* Show More button */}
+          {visibleCount < services.length && (
+            <div className="portfolio-showmore-container">
+              <button
+                className="portfolio-showmore-btn"
+                onClick={handleShowMore}
+              >
+                Show More
+              </button>
+            </div>
+          )}
+        </div>
+      )}
     </section>
   );
 };
