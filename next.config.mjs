c6d1ac7349a071a8d4307c0f883495e320bcabb5
MuhamanButt <muhamanibutt@gmail.com>
2025-08-16 12:10:39 +0500

    fix : fix eslint bugs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['127.0.0.1', 'localhost'], // add your backend host here, without protocol
-  },
-  eslint: {
-    ignoreDuringBuilds: true, // <- ignores ESLint warnings/errors during build
+    domains: ["127.0.0.1", "localhost"], // add your backend host here, without protocol
   },
 };
 
