f488e6807cdc1508e6178b38a89821233b20862e
tayyab2914 <tayyab1234rehman@gmail.com>
2025-08-16 10:26:27 +0500

    feat: add esline ignore

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ['127.0.0.1', 'localhost'], // add your backend host here, without protocol
   },
+  eslint: {
+    ignoreDuringBuilds: true, // <- ignores ESLint warnings/errors during build
+  },
 };
 
 export default nextConfig;
